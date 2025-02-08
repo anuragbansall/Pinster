82aa41e4d9d5063d98106ab759ddff91caa585be
Anurag Bansal <devanuragbansal@gmail.com>
2025-02-09 02:37:33 +0530

    Login/Register UI Done

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+module.exports = router;
